3f086f0d0f4dd96e76b0b7f6f33be19e7af10666
Will S. Medrano <will@wmedrano.dev>
2025-03-17 20:57:55 -0700

    Create type for tag.

--- src/object_manager.zig
+++ src/object_manager.zig
@@ -9,7 +9,7 @@ pub const ObjectManager = struct {
     pub fn deinit(self: *ObjectManager, allocator: std.mem.Allocator) void {
         self.symbols.deinit(allocator);
         for (self.lists.objects.items) |list_slot| {
-            allocator.free(list_slot.obj.list);
+            allocator.free(list_slot.list);
         }
         self.lists.deinit(allocator);
     }
@@ -17,56 +17,65 @@ pub const ObjectManager = struct {
 
 fn ObjectStorage(comptime T: type) type {
     return struct {
-        objects: std.ArrayListUnmanaged(TaggedObject(T)) = .{},
+        // Stores all objects. Objects may be referenced by index.
+        objects: std.ArrayListUnmanaged(T) = .{},
+        // Stores a tag. tags[idx] contains the tag for objects[idx]. A tag is
+	// assigned to each object to make sure that it is not referenced after
+	// garbage collection.
+        tags: std.ArrayListUnmanaged(Tag) = .{},
+	// Contains all indices that are available.
         available: std.ArrayListUnmanaged(ObjectId(T)) = .{},
+
         const Self = @This();
-        pub fn deinit(self: *Self, allocator: std.mem.Allocator) void {
+
+	pub fn deinit(self: *Self, allocator: std.mem.Allocator) void {
             self.objects.deinit(allocator);
+            self.tags.deinit(allocator);
             self.available.deinit(allocator);
         }
 
         pub fn put(self: *Self, allocator: std.mem.Allocator, obj: T) !ObjectId(T) {
             if (self.available.popOrNull()) |old_id| {
                 const id = ObjectId(T){
-                    .tag = old_id.tag +% 1,
+                    .tag = old_id.tag.next(),
                     .idx = old_id.idx,
                 };
-                self.objects.items[id.idx] = TaggedObject(T){
-                    .obj = obj,
-                    .tag = id.tag,
-                };
+                self.objects.items[id.idx] = obj;
+                self.tags.items[id.idx] = id.tag;
                 return id;
             }
             const id = ObjectId(T){
-                .tag = 0,
+                .tag = .{},
                 .idx = @intCast(self.objects.items.len),
             };
-            try self.objects.append(allocator, TaggedObject(T){
-                .obj = obj,
-                .tag = id.tag,
-            });
+            try self.objects.append(allocator, obj);
+            try self.tags.append(allocator, id.tag);
             return id;
         }
 
         pub fn get(self: *const Self, id: ObjectId(T)) ?*T {
-            if (self.objects.items[id.idx].tag != id.tag) {
+            if (!self.tags.items[id.idx].eql(id.tag)) {
                 return null;
             }
-            return &self.objects.items[id.idx].obj;
+            return &self.objects.items[id.idx];
         }
     };
 }
 
-fn TaggedObject(comptime T: type) type {
-    return struct {
-        obj: T,
-        tag: u8,
-    };
-}
+const Tag = packed struct {
+    id: u8 = 0,
+    pub fn next(self: Tag) Tag {
+        return Tag{ .id = self.id +% 1 };
+    }
+
+    pub fn eql(self: Tag, other: Tag) bool {
+        return self.id == other.id;
+    }
+};
 
 pub fn ObjectId(comptime _: type) type {
     return packed struct {
-        tag: u8,
+        tag: Tag,
         idx: u24,
     };
 }
